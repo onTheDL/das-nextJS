b97e93810aecb5a44b37ffb51a1db0470b271904
Daniel Laserna <daniel.laserna@protonmail.com>
2021-01-25 16:33:00 -0500

    Moved nearly completed das-development version to new computer.

---
+++ pages/_app.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import Theme from "../src/ui/Theme";
+import Header from "../src/ui/Header";
+import Footer from "../src/ui/Footer";
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  //States
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={Theme}>
+     
+        <Header
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
+        <Component
+          {...pageProps}
+          setValue={setValue}
+          setSelectedIndex={setSelectedIndex}
+        />
+        <Footer
+          setValue={setValue}
+          setSelectedIndex={setSelectedIndex}
+        />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
